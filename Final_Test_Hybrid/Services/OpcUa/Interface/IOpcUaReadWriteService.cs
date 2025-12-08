c30f13dcd1ad2c9e11047155eb104e1f212c5370
Alexander <ssss1231833@gmail.com>
2025-12-08 21:12:36 +0400

    Removed GC.SuppressFinalize call from Dispose method in OpcUaReadWriteService to adhere to best practices for resource management.

--- Final_Test_Hybrid/Services/OpcUa/Interface/IOpcUaReadWriteService.cs
+++
@@ -1,8 +0,0 @@
-namespace Final_Test_Hybrid.Services.OpcUa.Interface
-{
-    public interface IOpcUaReadWriteService
-    {
-        Task<T?> ReadNodeAsync<T>(string nodeId, CancellationToken ct = default);
-        Task WriteNodeAsync<T>(string nodeId, T value, CancellationToken ct = default);
-    }
-}
--- Final_Test_Hybrid/Services/OpcUa/Interface/IOpcUaSubscriptionService.cs
+++
@@ -1,8 +0,0 @@
-namespace Final_Test_Hybrid.Services.OpcUa.Interface
-{
-    public interface IOpcUaSubscriptionService : IAsyncDisposable
-    {
-        Task<bool> SubscribeAsync(string nodeId, Action<object?> callback);
-        Task UnsubscribeAsync(string nodeId);
-    }
-}
