0870e30f86c84cdc48f11c2f663bee3b2e254cfd
Alexander <ssss1231833@gmail.com>
2025-12-15 20:08:50 +0400

    Рефакторинг диалогового окна базы данных: выделение компонента для управления типами котлов и упрощение кода

---
+++ Final_Test_Hybrid/Models/Database/Edit/BoilerTypeEditModel.cs
@@ -0,0 +1,34 @@
+using Final_Test_Hybrid.Models.Database;
+
+namespace Final_Test_Hybrid.Models.Database.Edit;
+
+public class BoilerTypeEditModel
+{
+    public long Id { get; set; }
+    public string Article { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public int Version { get; set; }
+
+    public BoilerTypeEditModel()
+    {
+    }
+
+    public BoilerTypeEditModel(BoilerType entity)
+    {
+        Id = entity.Id;
+        Article = entity.Article;
+        Type = entity.Type;
+        Version = entity.Version;
+    }
+
+    public BoilerType ToEntity()
+    {
+        return new BoilerType
+        {
+            Id = Id,
+            Article = Article,
+            Type = Type,
+            Version = Version
+        };
+    }
+}
