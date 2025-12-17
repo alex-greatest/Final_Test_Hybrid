1a427aa9f12ce0575fe881226fb409f315c80d36
Alexander <ssss1231833@gmail.com>
2025-12-17 13:39:52 +0400

    Refactor StandDatabaseDialog to improve tab refresh logic; update event handling for boiler types and enhance tab refresh mechanism

--- Final_Test_Hybrid/Components/Engineer/StandDatabase/StandDatabaseDialog.razor.cs
+++ Final_Test_Hybrid/Components/Engineer/StandDatabase/StandDatabaseDialog.razor.cs
@@ -5,30 +5,40 @@ namespace Final_Test_Hybrid.Components.Engineer.StandDatabase;
 
 public partial class StandDatabaseDialog
 {
+    private const int RecipesTabIndex = 1;
+    private const int ResultSettingsTabIndex = 2;
+
     private int _selectedIndex;
     private RecipesGrid? _recipesGrid;
     private ResultSettingsTab? _resultSettingsTab;
-    private bool _needsRefresh;
+    private readonly HashSet<int> _tabsNeedingRefresh = [];
 
-    private void OnBoilerTypesChanged()
+    private void MarkDependentTabsForRefresh()
     {
-        _needsRefresh = true;
+        _tabsNeedingRefresh.Add(RecipesTabIndex);
+        _tabsNeedingRefresh.Add(ResultSettingsTabIndex);
     }
 
-    private async Task OnTabChanged(int index)
+    private async Task OnTabChanged(int tabIndex)
     {
-        if (!_needsRefresh)
+        var needsRefresh = _tabsNeedingRefresh.Remove(tabIndex);
+        if (!needsRefresh)
         {
             return;
         }
-        _needsRefresh = false;
-        if (index == 1 && _recipesGrid != null)
+        await RefreshTabContent(tabIndex);
+    }
+
+    private async Task RefreshTabContent(int tabIndex)
+    {
+        switch (tabIndex)
         {
-            await _recipesGrid.RefreshAsync();
-        }
-        if (index == 2 && _resultSettingsTab != null)
-        {
-            await _resultSettingsTab.RefreshAsync();
+            case RecipesTabIndex when _recipesGrid != null:
+                await _recipesGrid.RefreshAsync();
+                return;
+            case ResultSettingsTabIndex when _resultSettingsTab != null:
+                await _resultSettingsTab.RefreshAsync();
+                break;
         }
     }
 }
