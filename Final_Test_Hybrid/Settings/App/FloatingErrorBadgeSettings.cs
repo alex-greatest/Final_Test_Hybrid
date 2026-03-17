62910cf4b6bfc0204e354cb6ee227870acecba9e
Alexander <ssss1231833@gmail.com>
2026-03-17 17:26:05 +0400

    добавил настрйоки размеро икокни

---
+++ Final_Test_Hybrid/Settings/App/FloatingErrorBadgeSettings.cs
@@ -0,0 +1,11 @@
+namespace Final_Test_Hybrid.Settings.App;
+
+public class FloatingErrorBadgeSettings
+{
+    public int WidthPx { get; set; } = 84;
+    public int HeightPx { get; set; } = 84;
+    public int IconSizePx { get; set; } = 78;
+    public int CounterMinWidthPx { get; set; } = 24;
+    public int CounterHeightPx { get; set; } = 24;
+    public int CounterFontSizePx { get; set; } = 13;
+}
